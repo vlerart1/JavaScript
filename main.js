7a5c4f8f446aeeaf696a263bec69450648778e24
vlerart1 <167799326+vlerart1@users.noreply.github.com>
2025-05-10 04:27:15 -0700

    PRSH

---
+++ main.js
@@ -0,0 +1,18 @@
+console.log(num1 >= num3); // true
+console.log(num1 < num2);  // true
+console.log(num1 <= num2); // true
+
+var firstName = "Digital";
+var lastName = "School";
+
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+
+var test = "Test123";
+console.log(`Hello from ${test}`);
+
+var age = 22;
+var age2 = 24;
+console.log(age >= 18 && age <= 65); // and operator
+console.log(age >= 28 || age <= 65); // or operator
+console.log(!(age == age2));
